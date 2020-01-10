a1cbfd433f76c13e71224f41f16de7b6b1be9a33
nayrunner <Nutchanon_thonganun@cmu.co.th>
2020-01-10 15:13:13 +0700

    1

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main()
+{
+	double x,sum=0;
+	cout<<"Enter x: ";
+	cin>> x;
+	
+	while(x!=0){
+		if(x>0){sum=sum+x;
+		cout<<"Enter x: ";
+		cin>>x;
+		}
+		else{ cout<<"Enter x: ";
+		cin>>x;
+		}
+	}
+	
+	cout<<"sum = "<<sum;
+	return 0;
+}
